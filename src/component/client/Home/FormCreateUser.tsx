f15f19d132463c7810be7a99ea0aea5e07d9b907
goncang1512 <samuderanst@gmail.com>
2025-03-01 12:18:10 +0700

    fix: bug in home page client

--- src/component/client/Home/FormCreateUser.tsx
+++ src/component/client/Home/FormCreateUser.tsx
@@ -4,13 +4,22 @@ import { Button, Input } from "mogora-ui";
 import React from "react";
 
 function FormCreateUser() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    const response = await createUser(formData);
+    console.log(response); // Handle response (misalnya tampilkan pesan sukses atau error)
+  };
+
   return (
     <div>
-      <form action={createUser} className="flex flex-col gap-2 max-w-lg">
-        <Input name="name" type="text" placeholder="Username" />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2 max-w-lg">
+        <Input name="username" type="text" placeholder="Username" />
         <Input name="email" type="email" placeholder="Email" />
         <Input name="password" type="password" placeholder="Password" />
-        <Button>Submit</Button>
+        <Input name="confirm" type="password" placeholder="Confirm Password" />
+        <Button type="submit">Submit</Button>
       </form>
     </div>
   );
